12d632d84e5b376efcd715bcee423242ee0235f6
Natasha Gaffer <ngaffer@gmail.com>
2018-04-25 23:12:45 -0700

    mapped auth service response for signup method

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Http, Headers } from '@angular/http';
+import { Http, Headers, Response } from '@angular/http';
+import 'rxjs/Rx';
+import { Observable } from "rxjs";
 
 import { User } from './user.model';
 import { Plate } from './plate.model';
@@ -12,7 +14,9 @@ export class AuthService {
   signup(user: User) {
   	const body = JSON.stringify(user);
   	const headers = new Headers({'Content-Type': 'application/json'});
-  	return this.http.post('http://localhost:3000/user', body);
+  	return this.http.post('http://localhost:3000/user', body, {headers: headers})
+  		.map((response: Response) => response.json())
+  		.catch((error: Response) => Observable.throw(error.json()));
   }
 
   // addUser(user: User) {
